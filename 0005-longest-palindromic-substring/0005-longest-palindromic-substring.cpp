f00b535b44f033fc9d4ff3088e8509103aa2f7c0
shuklasarthak333-bit <shuklasarthak333@gmail.com>
2026-09-09 23:46:49 +0530

    Time: 6 ms (88.21%), Space: 9.4 MB (77.48%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int start = 0, maxLen = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            // Odd length palindrome
+            int l = i, r = i;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+                if(r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+                if(r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
